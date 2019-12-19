050fe70182aa3ade9d4d8751b03bdad5d336308b
Konstantin Ullrich <konstantinullrich12@gmail.com>
2019-12-19 23:30:18 +0100

    Add Blockchain request-wrapper

--- example/labcoin_sdk_exmaple.dart
+++ example/labcoin_sdk_exmaple.dart
@@ -1,9 +1,28 @@
 import 'package:labcoin_sdk/labcoin_sdk.dart';
 
 Future<void> main() async {
-  LabcoinClient labcoinClient =
-      LabcoinClient(LabcoinUri('localhost:3000'));
-  int stake = await labcoinClient
+  // Establish a Connection to a Labcoin Node
+  var labcoinClient = LabcoinClient(LabcoinUri('localhost:3000'));
+
+  // Request the Wallet Balance of a specific Wallet address
+  var stake = await labcoinClient
       .getWalletBalance('00000000000000000000000000000000000000000000');
-  print(stake);
+
+  // Get the last/newest Block of the Blockchain
+  var lastBlock = await labcoinClient.getNewestBlocks(1);
+
+  // Get the first/oldest Block of the Blockchain
+  var firstBlock = await labcoinClient.getOldestBlocks(1);
+
+  // Get the full Blockchain
+  var fullBlockchain = await labcoinClient.getFullBlockchain();
+
+  print('Stake: ${stake.toString()}');
+  print('---[START] Last Block ---');
+  print(lastBlock);
+  print('---[END] Last Block ---\n---[START] First Block ---');
+  print(firstBlock);
+  print('---[END] First Block ---\n---[START] Full Blockchain ---');
+  print(fullBlockchain);
+  print('---[END] Full Blockchain ---');
 }
--- lib/src/labcoin_client.dart
+++ lib/src/labcoin_client.dart
@@ -10,8 +10,35 @@ class LabcoinClient {
     print(nodeAddress.toString());
   }
 
+  Future<List> _sendBlockchainRequest(String pathSegment) async {
+    Response response =
+        await get('${nodeAddress.toString()}blockchain/$pathSegment');
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+    return null;
+  }
+
   void sendTransaction(Transaction transaction) =>
       post('${nodeAddress.toString()}transaction', body: transaction.toMap());
+
+  Future<List> getFullBlockchain() async => _sendBlockchainRequest('full');
+
+  Future<List> getNewestBlocks(int length) async {
+    if (!length.isNegative) {
+      length *= -1;
+    }
+    String lengthString = length.toString();
+    return _sendBlockchainRequest(lengthString);
+  }
+
+  Future<List> getOldestBlocks(int length) async {
+    if (length.isNegative) {
+      length *= -1;
+    }
+    String lengthString = length.toString();
+    return _sendBlockchainRequest(lengthString);
+  }
 
   Future<int> getWalletBalance(String walletAddress) async {
     Response response =

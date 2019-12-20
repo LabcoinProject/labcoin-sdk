f1fd8acb639b3b65f711a5363698658b293c2dae
Konstantin Ullrich <konstantinullrich12@gmail.com>
2019-12-20 18:43:56 +0100

    Add Wallet API

--- lib/labcoin_sdk.dart
+++ lib/labcoin_sdk.dart
@@ -1,3 +1,4 @@
 export 'src/labcoin_uri.dart';
 export 'src/transaction.dart';
+export 'src/wallet.dart';
 export 'src/labcoin_client.dart';
---
+++ lib/src/wallet.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:crypton/crypton.dart';
+import 'package:labcoin_sdk/src/pemhelper.dart';
+
+class Wallet {
+  ECPrivateKey _privateKey;
+
+  ECPublicKey get publicKey => _privateKey.publicKey;
+  ECPrivateKey get privateKey => _privateKey;
+
+  Wallet(String privateKey) {
+    _privateKey = ECPrivateKey.fromString(privateKey);
+  }
+
+  Wallet.fromRandom() {
+    var keypair = ECKeypair.fromRandom();
+    _privateKey = keypair.privateKey;
+  }
+
+  Wallet.fromPem(String privateKeyFilePath) {
+    var privateKeyFile = File(privateKeyFilePath);
+
+    if (!privateKeyFile.existsSync()) {
+      throw ('\"$privateKeyFilePath\" does not exist or is not a valid path');
+    }
+
+    _privateKey =
+        ECPrivateKey.fromString(decodePEM(privateKeyFile.readAsStringSync()));
+  }
+
+  void saveToFile(String folderPath) {
+    var directory = Directory(folderPath);
+    if (!directory.existsSync()) directory.createSync(recursive: true);
+
+    var privateKeyFile = File('${directory.path}/private_key');
+    var publicKeyFile = File('${directory.path}/public_key.pub');
+    if (!privateKeyFile.existsSync()) privateKeyFile.createSync();
+    if (!publicKeyFile.existsSync()) publicKeyFile.createSync();
+
+    privateKeyFile.writeAsString(encodePrivateKeyToPem(privateKey));
+    publicKeyFile.writeAsString(encodePublicKeyToPem(publicKey));
+  }
+}

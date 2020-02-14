d1e89742070e597c5388dc87a5b5b44b7e82bdd3
Konstantin Ullrich <konstantinullrich12@gmail.com>
2020-02-14 12:06:06 +0000

    Update labcoin.js

--- bin/labcoin.js
+++ bin/labcoin.js
@@ -1,5 +1,24 @@
-class Wallet {
-    constructor() {
+class LabcoinWallet {
+    constructor(privateKey = null) {
+        if (privateKey !== null) {
+            this.wallet = labcoin_createWallet(privateKey);
+        } else {
+            this.wallet = labcoin_createRandomWallet();
+        }
+    }
 
+    getPrivateKey() {return labcoin_getPrivateKey(this.wallet);}
+    getPublicKey() {return labcoin_getPublicAddress(this.wallet);}
+}
+
+class LabcoinTransaction {
+    constructor(fromAddress, toAddress, amount) {
+        this.transaction = labcoin_createTransaction(
+            fromAddress, toAddress, amount);
+    }
+
+    sign(privateKey) {
+        this.transaction = labcoin_signTransaction(
+            this.transaction, privateKey);
     }
 }

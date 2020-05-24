cb037012993e09e1029cfc41975fae3717b2e72f
Konstantin Ullrich <konstantinullrich12@gmail.com>
2020-05-24 17:41:11 +0200

    Small fix

--- lib/src/helper/labcoin_address.dart
+++ lib/src/helper/labcoin_address.dart
@@ -3,8 +3,8 @@ import 'package:labcoin_sdk/labcoin_sdk.dart';
 class LabcoinAddress{
   final String address;
   final int funds;
-  final List<Transaction> transactions;
-  final List<Transaction> memPoolTransactions;
+  final List<BlockDataType> transactions;
+  final List<BlockDataType> memPoolTransactions;
 
   LabcoinAddress(this.address, this.funds, this.transactions, this.memPoolTransactions);
 }
